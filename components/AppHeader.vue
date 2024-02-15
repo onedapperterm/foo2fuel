606b99ba7d06665416664d2f1491bc52c094f289
Julian <julian.castro@binaroo.com>
2024-02-15 17:11:38 +0100

    Project first commit.

---
+++ components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+const routes = ['home', 'business', 'families', 'tips']
+</script>
+
+<template>
+  <header>
+    <ul>
+      <li v-for="route in routes">
+          {{route}}
+      </li>
+    </ul>
+  </header>
+</template>
---
+++ pages/submit.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Submit</h1>
+  </div>
+</template>
